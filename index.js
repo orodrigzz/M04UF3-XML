2b90e7c6eb6ee6035c51b55d94898d5bfd32bf94
enti <enti@enti.enti>
2022-02-11 14:21:16 +0100

    Clase 11 Febrero

--- index.js
+++ index.js
@@ -4,29 +4,18 @@ let http = require("http");
 let fs = require("fs");
 
 let http_server = http.createServer(function(req, res){
-
-	if (req.url == "/character.png"){
-	 fs.readFile("character.png", function(err,data){
-	if (req.url == "/formula.png"){
-	 fs.readFile("formula.png", function(err,data){
-	if (err){
-		console.log("Error");
-		return
-		}
-		res.writeHead(200);
-		res.end(data);
-		});
-		return
+	var archivo = "index.html";
+	if (req.url != "/"){
+		archivo = req.url.substring(1);	
 	}
 
-	fs.readFile("index.html", function(err,data){
-	if (err){
-		console.log("Error");
-		return
+	fs.readFile(archivo, function(err,data){
+		if (err){
+			console.log("Error");
+			return
 		}
-
 		res.writeHead(200);
 		res.end(data);
-		});
+	});
 
-		}).listen(1095);
+	}).listen(1095);
